9fb36a60297f4872820572d1cc3cd6ed94814b51
Illia <illiasychov@gmail.com>
2025-09-20 21:36:00 +0300

    Add json file for DataMigrationApp

--- Airbnb.Application/DTO/Migrations/ExternalApartment.cs
+++ Airbnb.Application/DTO/Migrations/ExternalApartment.cs
@@ -5,4 +5,7 @@ public record ExternalApartment
     public string ExternalId { get; set; } 
     public string Title { get; set; }
     public string Description { get; set; }
+    public string Location { get; set; }
+    public decimal Price { get; set; } 
+    public string Metadata { get; set; }
 }
