cb33d7d5a465bf7e53685c83e0b3e6afca7283b1
Illia <illiasychov@gmail.com>
2025-10-08 19:25:08 +0300

    One changes in unit test method

--- Airbnb.Domain/ValueObject/DateRange.cs
+++ Airbnb.Domain/ValueObject/DateRange.cs
@@ -13,4 +13,9 @@ public record DateRange
         Start = DateTime.SpecifyKind(start, DateTimeKind.Utc);
         End = DateTime.SpecifyKind(end, DateTimeKind.Utc);
     }
+
+    public bool Intersect(DateRange date)
+    {
+        return Start < date.End && End > date.Start;
+    }
 }
